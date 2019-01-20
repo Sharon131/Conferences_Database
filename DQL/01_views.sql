0fff9bb69bf15eb4df521e95423ffdf2ad61c81e
Piotr Kasprzyk <kaspiotr@gmail.com>
2019-01-20 18:46:04 +0100

    Add a view for checking the number of places left per conference day

--- DQL/01_views.sql
+++ DQL/01_views.sql
@@ -1,2 +1,15 @@
 USE ConferencesDB
 GO
+
+CREATE VIEW V_ATTENDEES_COUNT_PER_DAY
+AS
+    SELECT cf.[Description], cd.ConferenceDayID, cd.Day, SeatNo, COUNT(cat.ConferenceAttendeeID) AS attendees_number
+    FROM ConferencesDays AS cd
+    LEFT OUTER JOIN Conferences AS cf
+    ON cf.ConferenceID = cd.ConferenceID
+    LEFT OUTER JOIN ConferencesReservations AS cr
+    ON cr.ConferenceDayID = cd.ConferenceDayID
+    LEFT OUTER JOIN ConferencesAttendees cat
+    ON cat.ConferenceReservationID = cr.ConferenceReservationID
+    GROUP BY cf.[Description], cd.ConferenceDayID, cd.Day, SeatNo
+GO
