4fe36fe1140c792695a648a4c9eff040fe6674ed
Piotr Kasprzyk <kaspiotr@gmail.com>
2019-01-20 13:21:29 +0100

    Change insert into order (for easier debugging)
          * clean up scripts

--- create_database.bat
+++ create_database.bat
@@ -1,7 +1,8 @@
 sqlcmd -S %1 -i DDL\01_tables.sql
 sqlcmd -S %1 -i DDL\02_constraints.sql
 sqlcmd -S %1 -i DDL\03_triggers.sql
-sqlcmd -S %1 -i DDL\04_procedures.sql
+sqlcmd -S %1 -i DDL\04_indexes.sql
+sqlcmd -S %1 -i DDL\05_procedures.sql
 
 sqlcmd -S %1 -i DQL\01_views.sql
 
