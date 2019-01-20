4a79fcc7c42bc19afd7e2c8b7d1fa137a2a8a35c
Piotr Kasprzyk <kaspiotr@gmail.com>
2019-01-20 23:33:49 +0100

    Add a procedure to check if the attendee id is already on the ...
          ... workshop's list

--- DDL/06_procedures.sql
+++ DDL/06_procedures.sql
@@ -1,2 +1,38 @@
 USE ConferencesDB
 GO
+
+-- jak nie zwroci zera rekordow to procedura powinna przerwac transakcje
+-- proba dodania uczestnika drugi raz na ten sam warsztat
+-- EXECUTE proc_addAttendeeToWorkshopIfNotAssigned 272, 21
+
+IF OBJECT_ID ( 'dbo.proc_addAttendeeToWorkshopIfNotAssigned', 'P' ) IS NOT NULL
+    DROP PROCEDURE dbo.proc_addAttendeeToWorkshopIfNotAssigned;
+GO
+
+CREATE PROCEDURE dbo.proc_addAttendeeToWorkshopIfNotAssigned
+    @conferenceAttendeeId INT,
+    @workshopId INT
+AS
+BEGIN
+SET NOCOUNT ON
+IF EXISTS
+    (SELECT wat.ConferenceAttendeeID, wat.WorkshopAttendeeID, cd.ConferenceDayID, ws.WorkshopID
+    FROM ConferencesAttendees  AS cat
+    JOIN ConferencesReservations AS cr
+    ON cr.ConferenceReservationID = cat.ConferenceReservationID
+    JOIN ConferencesDays AS cd
+    ON cd.ConferenceDayID = cr.ConferenceDayID
+    JOIN WorkshopsAttendees AS wat
+    ON wat.ConferenceAttendeeID = cat.ConferenceAttendeeID
+    JOIN Workshops AS ws
+    ON ws.ConferenceDayID = cd.ConferenceDayID
+    WHERE wat.ConferenceAttendeeID = @conferenceAttendeeId -- parametr
+    	AND ws.WorkshopID = @workshopId -- 2 parametr
+   )
+  RAISERROR('Attendee with id %d is already assigned to the workshop %d', 16, 1,
+			@conferenceAttendeeId,@workshopId)
+
+ELSE
+   PRINT FORMATMESSAGE('Attendee %d added to %d', @conferenceAttendeeId, @workshopId)
+END;
+GO
