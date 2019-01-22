d74c94be6a5c2ed870ab95ba6b18fe44b6ba9bd5
Piotr Kasprzyk <kaspiotr@gmail.com>
2019-01-22 23:13:10 +0100

    Add index to on Name in Customer table

--- DDL/05_indexes.sql
+++ DDL/05_indexes.sql
@@ -1,2 +1,11 @@
 USE ConferencesDB
 GO
+
+IF ((SELECT COUNT(*)
+FROM sys.indexes
+WHERE name='idx_customername' AND object_id = OBJECT_ID('dbo.Customers')) = 1)
+DROP INDEX idx_customername ON Customers
+GO
+
+CREATE INDEX idx_customername
+ON Customers (Name);
